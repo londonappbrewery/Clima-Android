71313903cea72d561350d34fc6a70f43528f5e5b
londonappbrewery <londonappbrewery@gmail.com>
2016-01-21 15:00:53 +0000

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -0,0 +1,76 @@
+package com.londonappbrewery.climapm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class WeatherController extends AppCompatActivity {
+
+    // Constants:
+    String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    // App ID to use OpenWeather data
+    String APP_ID = "e72ca729af228beabd5d20e3b7749713";
+    // Time between location updates (5000 milliseconds or 5 seconds)
+    long MIN_TIME = 5000;
+    // Distance between location updates (1000m or 1km)
+    float MIN_DISTANCE = 1000;
+
+    // TODO: Set LOCATION_PROVIDER here:
+
+
+
+
+    // Member Variables:
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
+
+    // TODO: Declare a LocationManager and a LocationListener here:
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_controller_layout);
+
+        // Linking the elements in the layout to Java code
+        mCityLabel = (TextView) findViewById(R.id.locationTV);
+        mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
+        mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
+        ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
+
+
+
+        // TODO: Add an OnClickListener to the changeCityButton here:
+
+    }
+
+
+    // TODO: Add onResume() here:
+
+
+
+    // TODO: Add getWeatherForNewCity(String city) here:
+
+
+
+    // TODO: Add getWeatherForCurrentLocation() here:
+
+
+
+    // TODO: Add letsDoSomeNetworking(RequestParams params) here:
+
+
+
+    // TODO: Add updateUI() here:
+
+
+
+    // TODO: Add onPause() here:
+
+
+
+}
---
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherDataModel.java
@@ -0,0 +1,47 @@
+package com.londonappbrewery.climapm;
+
+public class WeatherDataModel {
+
+    // TODO: Declare the member variables here
+
+
+    // TODO: Create a WeatherDataModel from a JSON:
+
+
+    // TODO: Uncomment to this to get the weather image name from the condition:
+//    private static String updateWeatherIcon(int condition) {
+//
+//        if (condition >= 0 && condition < 300) {
+//            return "tstorm1";
+//        } else if (condition >= 300 && condition < 500) {
+//            return "light_rain";
+//        } else if (condition >= 500 && condition < 600) {
+//            return "shower3";
+//        } else if (condition >= 600 && condition <= 700) {
+//            return "snow4";
+//        } else if (condition >= 701 && condition <= 771) {
+//            return "fog";
+//        } else if (condition >= 772 && condition < 800) {
+//            return "tstorm3";
+//        } else if (condition == 800) {
+//            return "sunny";
+//        } else if (condition >= 801 && condition <= 804) {
+//            return "cloudy2";
+//        } else if (condition >= 900 && condition <= 902) {
+//            return "tstorm3";
+//        } else if (condition == 903) {
+//            return "snow5";
+//        } else if (condition == 904) {
+//            return "sunny";
+//        } else if (condition >= 905 && condition <= 1000) {
+//            return "tstorm3";
+//        }
+//
+//        return "dunno";
+//    }
+
+    // TODO: Create getter methods for temperature, city, and icon name:
+
+
+
+}
